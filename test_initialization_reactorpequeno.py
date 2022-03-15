7d2868ee8b3042e17efb44acc38657a80b1ad405
David-Linan <dada2828@gmail.com>
2022-03-15 13:20:28 +0100

    results

--- test_initialization_reactorpequeno.py
+++ test_initialization_reactorpequeno.py
@@ -718,8 +718,8 @@ if __name__ == "__main__":
 
     ##used in second test test_probabilities_more_rigurous2_5_to_7
     size_of_batches=10
-    initial_sampled_points=11
-    max_number_sampled_points=13
+    initial_sampled_points=14
+    max_number_sampled_points=16
     value_slightly_greater_than_global_opt=3.08
     tolerance=0.01
     number_of_iter_where_probability_must_remain_constant=10
@@ -773,11 +773,11 @@ if __name__ == "__main__":
                     break        
     probability_data=[accum_probability,accum_average_cpu,accum_multi_cpu,accum_solver_cpu]
 
-    a_file = open("test_probabilities_more_rigurous2_11_to_13.pkl", "wb")
+    a_file = open("test_probabilities_more_rigurous2_14_to_16.pkl", "wb")
     pickle.dump(probability_data, a_file)
     a_file.close()
 
-    a_file = open("test_probabilities_secondary_info_more_rigurous2_11_to_13.pkl", "wb")
+    a_file = open("test_probabilities_secondary_info_more_rigurous2_14_to_16.pkl", "wb")
     pickle.dump(runs_dict, a_file)
     a_file.close()
 
