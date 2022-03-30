75e38f664ee524095cbc2a5abcdeba5585da22fa
David-Linan <dada2828@gmail.com>
2022-03-30 15:27:02 +0200

    update

--- test_initialization_reactorpequeno_naive.py
+++ test_initialization_reactorpequeno_naive.py
@@ -734,11 +734,11 @@ if __name__ == "__main__":
     
     ##used in third test test_probabilities_more_rigurous4
     size_of_batches=10
-    initial_sampled_points=23
-    max_number_sampled_points=24
+    initial_sampled_points=25
+    max_number_sampled_points=26
     value_slightly_greater_than_global_opt=3.08
     tolerance=0.01
-    number_of_iter_where_probability_must_remain_constant=40
+    number_of_iter_where_probability_must_remain_constant=2
     
     print("initial_sampled_points",initial_sampled_points)
     print("size_of_batches",size_of_batches)
@@ -788,11 +788,11 @@ if __name__ == "__main__":
                     break        
     probability_data=[accum_probability,accum_average_cpu,accum_multi_cpu,accum_solver_cpu]
 
-    a_file = open("test_probabilities_more_rigurous4_23_to_24_naive.pkl", "wb")
+    a_file = open("test_probabilities_more_rigurous4_25_to_26_naive.pkl", "wb")
     pickle.dump(probability_data, a_file)
     a_file.close()
 
-    a_file = open("test_probabilities_secondary_info_more_rigurous4_23_to_24_naive.pkl", "wb")
+    a_file = open("test_probabilities_secondary_info_more_rigurous4_25_to_26_naive.pkl", "wb")
     pickle.dump(runs_dict, a_file)
     a_file.close()
 
