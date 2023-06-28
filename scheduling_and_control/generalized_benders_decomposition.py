1fc343e72d27526a0944ce187447c0b593ab725c
David-Linan <dada2828@gmail.com>
2023-06-27 21:21:36 -0400

    changes

--- scheduling_and_control/generalized_benders_decomposition.py
+++ scheduling_and_control/generalized_benders_decomposition.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     minlp_solver='DICOPT'
     nlp_solver='conopt4'
     transform='bigm'
+    Infinity_aprox=100000
 
 
     if minlp_solver=='dicopt' or minlp_solver=='DICOPT':
@@ -56,7 +57,7 @@ if __name__ == "__main__":
 
     logic_fun=problem_logic_scheduling_case1
     model_fun=scheduling_and_control_gdp_N_GBD
-    
+    ###### MATER PROBLEM ##################
     mas=model_fun(**kwargs2) #master problem
     mas.cuts=pe.ConstraintList() #Initialize benders cuts
 
@@ -87,7 +88,7 @@ if __name__ == "__main__":
     transformation_string = 'gdp.' + transform
     pe.TransformationFactory(transformation_string).apply_to(mas)
 
-
+    ###### SUBPROBLEM ##################
     sub=model_fun(**kwargs2) #subproblem
     sub.E2_CAPACITY_LOW.deactivate()
     sub.E2_CAPACITY_UP.deactivate()
@@ -104,11 +105,12 @@ if __name__ == "__main__":
                 disjunct=sub.YR_disjunct[I,J][disj]
                 
                 for constr in disjunct.component_objects(pe.Constraint, descend_into=True):
-                    if constr.local_name=='DEF_VAR_TIME' or constr.local_name=='DEF_AUX1' or constr.local_name=='DEF_AUX2':
+                    if constr.local_name=='DEF_VAR_TIME' or constr.local_name=='DEF_AUX1' or constr.local_name=='DEF_AUX2': #NOTE that I am deactivating everythin. I am just using this to show that I can compare by name to deactivate what I want
                         constr.deactivate()
 
             sub.YR_disjunct[I,J].deactivate()
             sub.Disjunction1[I,J].deactivate()
+            sub.oneYR[I,J].deactivate()
 
     sub.X_Z_relation.deactivate()
 
@@ -124,6 +126,11 @@ if __name__ == "__main__":
     #     sub.oneYR2[I,J].deactivate()
     #     sub.Disjunction2[I,J].deactivate()
 
+
+    sub.obj.deactivate()
+    def _obj_dynamic(m):
+        return m.TCP3
+    sub.obj_dyn = pe.Objective(rule=_obj_dynamic, sense=pe.minimize) 
 
     #linking variables and constraints in subproblem:
     sub.link_B=pe.Var(sub.I,sub.J,sub.T,within=pe.NonNegativeReals)
@@ -144,19 +151,45 @@ if __name__ == "__main__":
     # Dual variables
     sub.dual = pe.Suffix(direction=pe.Suffix.IMPORT) #define dual variables
 
-       
-    
-    # ext_ref={m.YR[I,J]:m.ordered_set[I,J] for I in m.I_reactions for J in m.J_reactors if m.I_i_j_prod[I,J]==1}
-    # ext_ref.update({m.YR2[I_J]:m.ordered_set2[I_J] for I_J in m.I_J})
-    # [reformulation_dict, number_of_external_variables, lower_bounds, upper_bounds]=get_external_information(m,ext_ref,tee=True)
 
+    ###### FEASIBILITY SUBPROBLEM ##################
+    feas=sub.clone()
+    sum_infeasibility=0  #sum of infeasibility
+    feas.elastic_vars={}
 
-    ## RUN THIS TO SOLVE
+    count_elastic=0
+    for constr in feas.component_data_objects(ctype=pe.Constraint, active=True, descend_into=True):
+        if constr.parent_component().name != 'const_link_B' and constr.parent_component().name != 'const_link_VarTime': 
+            if constr.equality:
+                count_elastic=count_elastic+1
+                feas.elastic_vars[count_elastic]=pe.Var(within=pe.NonNegativeReals,initialize=0)
+                setattr(feas,'elastic_vars_%s' %str(count_elastic),feas.elastic_vars[count_elastic])
+                constr._body+=feas.elastic_vars[count_elastic]
 
-    #TESTS
-    mas=initialize_model(mas,from_feasible=True,feasible_model='case_1_scheduling_and_dynamics_solution')
-    
-    for v in mas.component_objects(pe.Var, descend_into=True): #test: master problem initialized with fixed linking variabels that guaranteee feasibility
+                count_elastic=count_elastic+1
+                feas.elastic_vars[count_elastic]=pe.Var(within=pe.NonNegativeReals,initialize=0)
+                setattr(feas,'elastic_vars_%s' %str(count_elastic),feas.elastic_vars[count_elastic])
+                constr._body+=-feas.elastic_vars[count_elastic]
+            else:
+                count_elastic=count_elastic+1
+                if constr.has_lb():
+                    feas.elastic_vars[count_elastic]=pe.Var(within=pe.NonNegativeReals,initialize=0)
+                    setattr(feas,'elastic_vars_%s' %str(count_elastic),feas.elastic_vars[count_elastic])
+                    constr._body+=feas.elastic_vars[count_elastic]
+                if constr.has_ub():
+                    feas.elastic_vars[count_elastic]=pe.Var(within=pe.NonNegativeReals,initialize=0)
+                    setattr(feas,'elastic_vars_%s' %str(count_elastic),feas.elastic_vars[count_elastic])
+                    constr._body+=-feas.elastic_vars[count_elastic]
+        # constr.pprint()
+    feas.obj_dyn.deactivate()
+    def _obj_feas(m):
+        return sum( feas.elastic_vars[i]  for i in feas.elastic_vars.keys())
+    feas.obj_feas = pe.Objective(rule=_obj_feas, sense=pe.minimize)     
+    # sub.obj_feas.pprint()
+
+    #TESTS-----------------------------------------------------------------------------------------------------------------------------------------------------
+    mas=initialize_model(mas,from_feasible=True,feasible_model='case_1_scheduling_and_dynamics_solution') #THIS IS THE SEQUENTIAL NAIVE APPROACH, WHIH RETURNS THE SAME SOLUTION AS THE SEQUENTIAL ITERATIVE APPROACH IN THIS CASE
+    for v in mas.component_objects(pe.Var, descend_into=True): #test: master problem initialized with fixed linking variabels that guaranteee feasibility (NOTE: this is jsut a test to see what happens!!!)
         if v.name=='varTime' or v.name=='B':
             for index in v:
                 if index==None:
@@ -164,11 +197,12 @@ if __name__ == "__main__":
                 else:
                     v[index].fix(pe.value(v[index]))
 
-    mas=solve_with_minlp(mas,transformation='',minlp='cplex',timelimit=86400,gams_output=False,tee=False,rel_tol=0,transform_required=False)
+    mas=solve_with_minlp(mas,transformation='',minlp='cplex',minlp_options=sub_options,timelimit=86400,gams_output=False,tee=False,rel_tol=0,transform_required=False)
+    print(pe.value(mas.obj))
     # mas.varTime.pprint()
     # mas.B.pprint()
 
-    # fix variables in subproblem
+    # fix variables in subproblem 
     for I in sub.I:
         for J in sub.J:
             for T in sub.T:
@@ -189,7 +223,7 @@ if __name__ == "__main__":
             sub.link_varTime[I,J].fix(pe.value(mas.varTime[I,J]))  
 
 
-    sub=solve_subproblem(sub,subproblem_solver='conopt4',subproblem_solver_options = {},timelimit = 86400, gams_output = False,tee = True,rel_tol = 0)
+    sub=solve_subproblem(sub,subproblem_solver='conopt4',subproblem_solver_options = sub_options,timelimit = 86400, gams_output = False,tee = False,rel_tol = 0)
     #print dual info
     # for c in sub.component_objects(pe.Constraint, active=True):
     #     if c.name=='const_link_B' or c.name=='const_link_VarTime':
@@ -203,13 +237,221 @@ if __name__ == "__main__":
     
     if sub.dsda_status=='Optimal':
         mas.cuts.add(sum(sum( sub.dual[sub.const_link_VarTime[I,J]]*(mas.varTime[I,J]-pe.value(sub.link_varTime[I,J])) for I in mas.I_reactions)  for J in mas.J_reactors)+sum(sum(sum(  sub.dual[sub.const_link_B[I,J,T]]*( mas.B[I,J,T]-pe.value(sub.link_B[I,J,T])   )      for I in mas.I) for J in mas.J) for T in mas.T)+pe.value(sub.TCP3)<=mas.TCP3)
+        print('Optimality cut to initialize: ')
+        mas.cuts.pprint()
+        
+        TPC1=pe.value(sub.TCP1)
+        TPC2=pe.value(sub.TCP2)
+        TPC3=pe.value(sub.TCP3)
+        TMC=pe.value(sub.TMC)
+        SALES=pe.value(sub.SALES)
+        sub.UBD=TPC1+TPC2+TPC3+TMC-SALES
     else:
+
+        #TODO: rewrite!!
         mas.cuts.add(sum(sum( sub.dual[sub.const_link_VarTime[I,J]]*(mas.varTime[I,J]-pe.value(sub.link_varTime[I,J])) for I in mas.I_reactions)  for J in mas.J_reactors)+sum(sum(sum(  sub.dual[sub.const_link_B[I,J,T]]*( mas.B[I,J,T]-pe.value(sub.link_B[I,J,T])   )      for I in mas.I) for J in mas.J) for T in mas.T)<=0)
-    
-    mas.cuts.pprint()
+
+        print('Feasibility cut to initialie:')
+        mas.cuts.pprint()
+        sub.UBD=Infinity_aprox
+
+    # make sure that variables in master are left unfixed
+    for v in mas.component_objects(pe.Var, descend_into=True): #test: master problem initialized with fixed linking variabels that guaranteee feasibility (NOTE: this is jsut a test to see what happens!!!)
+        if v.name=='varTime' or v.name=='B':
+            for index in v:
+                if index==None:
+                    v.unfix()
+                else:
+                    v[index].unfix()
+
+    # END OF TEST--------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# GENERALIZED BENDERS DECOMPOSITION-------------------------------------------------------------------------------------------------------------------------
+
+    start=time.time()
+    max_iter=1000
+    epsilon=0
+    tol=1e-6 #feasibility tolearance
+    for k in range(max_iter):
+        print('------------------------Iteration ',str(k),'------------------------------------')
+    #1: solve the master problem
+        mas=solve_with_minlp(mas,transformation='',minlp='cplex',minlp_options=sub_options,timelimit=86400,gams_output=False,tee=False,rel_tol=0,transform_required=False)
+        mas.LBD=pe.value(mas.obj)
+
+    #2: verify stopping criterion
+        current=time.time()
+        print('Primal obj: ',str(sub.UBD),'Master obj:', str(mas.LBD),'Current time: ',str(current-start))
+        if sub.UBD- mas.LBD <=epsilon:
+            break
+
+    #3: Solve primal problem
+        # fix variables in subproblem 
+        for I in sub.I:
+            for J in sub.J:
+                for T in sub.T:
+                    sub.link_B[I,J,T].fix(pe.value(mas.B[I,J,T]))
+                    sub.X[I,J,T].fix(round(pe.value(mas.X[I,J,T])))
+
+        for I_J in sub.I_J:
+                I=I_J[0]
+                J=I_J[1]
+                sub.Nref[I,J].fix(round(pe.value(mas.Nref[I,J])))
+
+        for K in sub.K:
+            for T in sub.T:
+                sub.S[K,T].fix(pe.value(mas.S[K,T]))
+
+        for I in sub.I_reactions:
+            for J in sub.J_reactors:
+                sub.link_varTime[I,J].fix(pe.value(mas.varTime[I,J]))    
+
+        # solve primal (subprolem)
+        sub=solve_subproblem(sub,subproblem_solver='conopt4',subproblem_solver_options = sub_options,timelimit = 86400, gams_output = False,tee = False,rel_tol = 0)     
+        print('Subproblem status:',sub.dsda_status)
+        if sub.dsda_status=='Optimal':
+            
+            
+            
+            TPC1=pe.value(sub.TCP1)
+            TPC2=pe.value(sub.TCP2)
+            TPC3=pe.value(sub.TCP3)
+            TMC=pe.value(sub.TMC)
+            SALES=pe.value(sub.SALES)
+
+            sub.UBD=min([sub.UBD,TPC1+TPC2+TPC3+TMC-SALES])
+
+            # if sub.UBD- mas.LBD <=epsilon:
+            #     break
+
+            mas.cuts.add(sum(sum( sub.dual[sub.const_link_VarTime[I,J]]*(mas.varTime[I,J]-pe.value(sub.link_varTime[I,J])) for I in mas.I_reactions)  for J in mas.J_reactors)+sum(sum(sum(  sub.dual[sub.const_link_B[I,J,T]]*( mas.B[I,J,T]-pe.value(sub.link_B[I,J,T])   )      for I in mas.I) for J in mas.J) for T in mas.T)+pe.value(sub.TCP3)<=mas.TCP3)
+            print('Optimaliti cut: ')
+            mas.cuts.pprint()
+            
+        else:
+
+            # sum_infeasibility=0  #sum of infeasibility
+            # # infeasible_const=[]  #infeasible constraints
+
+            # for constr in sub.component_data_objects(ctype=pe.Constraint, active=True, descend_into=True):
+            #     constr_body_value = pe.value(constr.body, exception=False)
+            #     constr_lb_value = pe.value(constr.lower, exception=False)
+            #     constr_ub_value = pe.value(constr.upper, exception=False)
+
+            #     constr_undefined = False
+            #     equality_violated = False
+            #     lb_violated = False
+            #     ub_violated = False
+
+            #     if constr_body_value is None:
+            #         # Undefined constraint body value due to missing variable value
+            #         constr_undefined = True
+            #         pass
+            #     else:
+            #         # Check for infeasibilities
+            #         if constr.equality:
+            #             if fabs(constr_lb_value - constr_body_value) >= tol:
+            #                 equality_violated = True
+            #                 sum_infeasibility=sum_infeasibility+fabs(constr_lb_value - constr_body_value)
+            #         else:
+            #             if constr.has_lb() and constr_lb_value - constr_body_value >= tol:
+            #                 lb_violated = True
+            #                 sum_infeasibility=sum_infeasibility+fabs(constr_lb_value - constr_body_value)
+            #             if constr.has_ub() and constr_body_value - constr_ub_value >= tol:
+            #                 ub_violated = True
+            #                 sum_infeasibility=sum_infeasibility+fabs(constr_body_value - constr_ub_value)
+            #     if not any((constr_undefined, equality_violated, lb_violated, ub_violated)):
+            #         # constraint is fine. skip to next constraint
+            #         continue
+
+            #     # output_dict = dict(name=constr.name)
+            #     # infeasible_const.append(output_dict)
+            
+            # sum_infeasibility=0
+            # for c in sub.component_objects(pe.Constraint, active=True):
+            #     # print(c.local_name)
+            #     if c.local_name != 'const_link_B' and c.local_name != 'const_link_VarTime':
+            #         for index in c:
+            #             if index==None:
+            #                 # print(c.body)
+            #                 # print(c.name,index,str(sub.dual[c]))
+            #                 sum_infeasibility=sum_infeasibility+sub.dual[c]*pe.value(c.body)
+            #             else:
+            #                 # print(c[index].body)
+            #                 # print(c.name,index,str(sub.dual[c[index]]))   
+            #                 sum_infeasibility=sum_infeasibility+sub.dual[c[index]]*pe.value(c[index].body)
+
+
+
+
+
+
+            # fix variables in subproblem 
+            for I in feas.I:
+                for J in feas.J:
+                    for T in feas.T:
+                        feas.link_B[I,J,T].fix(pe.value(mas.B[I,J,T]))
+                        feas.X[I,J,T].fix(round(pe.value(mas.X[I,J,T])))
+
+            for I_J in feas.I_J:
+                    I=I_J[0]
+                    J=I_J[1]
+                    feas.Nref[I,J].fix(round(pe.value(mas.Nref[I,J])))
+
+            for K in feas.K:
+                for T in feas.T:
+                    feas.S[K,T].fix(pe.value(mas.S[K,T]))
+
+            for I in feas.I_reactions:
+                for J in feas.J_reactors:
+                    feas.link_varTime[I,J].fix(pe.value(mas.varTime[I,J]))  
+
+
+            feas=initialize_model(feas,from_feasible=True,feasible_model='case_1_scheduling_and_dynamics_solution')# TODO: added this because I found an error!!!
+            feas=solve_subproblem(feas,subproblem_solver='conopt4',subproblem_solver_options = sub_options,timelimit = 86400, gams_output = False,tee = False,rel_tol = 0)     
+            print('feasproblem status:',feas.dsda_status, feas.results.solver.termination_condition)
+            if feas.dsda_status=='Optimal':
+                sum_infeasibility=pe.value(feas.obj_feas)
+                mas.cuts.add(sum(sum( feas.dual[feas.const_link_VarTime[I,J]]*(mas.varTime[I,J]-pe.value(feas.link_varTime[I,J])) for I in mas.I_reactions)  for J in mas.J_reactors)+sum(sum(sum(  feas.dual[feas.const_link_B[I,J,T]]*( mas.B[I,J,T]-pe.value(feas.link_B[I,J,T])   )      for I in mas.I) for J in mas.J) for T in mas.T)+sum_infeasibility<=0)
+                print('Feasibility cut:')
+                mas.cuts.pprint()
+            else:
+                print('Problem with feasibility stage')
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# END OF GENERALIZED BENDERS DECOMPOSITION---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
     ## RUN THIS TO RETRIEVE SOLUTION    
 
     # m=initialize_model(m,from_feasible=True,feasible_model='case_1_generalized_benders_Test')
+
+
+
+
+
+
+
 
 
     Sol_found=[]
